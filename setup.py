22ba2287b8efa66b2b96a4845dbc1605e793ac73
abdulrah77 <abdulrahuman1631@gmail.com>
2022-12-26 00:12:17 +0530

    created notebook, setup and requiremnets

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="abdulrah77",
+    author_email="abdulrahuman1631@gmail.com",
+    packages = find_packages(),
+    install_requires =  get_requirements(),
+
+)
