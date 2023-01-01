977f7dbbebb7f7084146ed0e3eadfdd354ddcd54
abdulrah77 <abdulrahuman1631@gmail.com>
2023-01-02 01:55:42 +0530

    completed dev code

--- main.py
+++ main.py
@@ -9,35 +9,12 @@ from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
 from sensor.components.model_evaluation import ModelEvaluation
-
+from sensor.components.model_pusher import ModelPusher
+from sensor.pipeline import start_training_pipeline
 
 if __name__ =="__main__":
 
     try:
-        training_pipeline_config =   config_entity.TrainingPipelineConfig()
-        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        print(data_ingestion_config.to_dict())
-        data_ingestion = data_ingestion.DataIngestion(data_ingestion_config = data_ingestion_config)
-        data_ingestion_artifact = data_ingestion.inititate_data_ingestion()
-
-
-        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config= training_pipeline_config)
-        data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact = data_ingestion_artifact)
-        data_validation_artifact = data_validation.initiate_data_validation()
-
-        data_tranformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
-        data_tranformation = DataTransformation(data_transformation_config=data_tranformation_config, data_ingestion_artifact=data_ingestion_artifact)
-        data_transformation_artifact = data_tranformation.initiate_data_transformation()
-
-
-        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
-        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
-        model_trainer_artifact = model_trainer.initiate_model_trainer()
-
-
-        model_eval_config= config_entity.ModelEvaluationConfig(training_pipeline_config= training_pipeline_config)
-        model_eval= ModelEvaluation(model_eval_config=model_eval_config, data_ingestion_artifact=data_ingestion_artifact, data_transformation_artifact=data_transformation_artifact, model_trainer_artifact=model_trainer_artifact)
-        model_eval_artifact = model_eval,initiate_model_evaluation()
-
+        start_training_pipeline()
     except Exception as e:
         raise SensorException(e, sys)
